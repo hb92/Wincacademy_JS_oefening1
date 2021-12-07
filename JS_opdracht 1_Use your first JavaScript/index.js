c1eb69f30ba498a938eab54a17642205e18ce59a
hb92 <britt.hellemans@gmail.com>
2021-12-07 13:54:50 +0100

    Add files via upload

---
+++ JS_opdracht 1_Use your first JavaScript/index.js
@@ -0,0 +1,35 @@
+console.log("Hello Winc Academy")
+
+// Name 
+let name = 'Britt';
+console.log(name);
+
+// Nee een if variable gaat niet want is een keyword
+
+// Addition
+let y = 5;
+let x = y+2;
+console.log(x);
+
+// Subtraction
+let a = y-2;
+console.log(a);
+
+// Multiplication
+let b =y*2;
+console.log(b);
+
+// Division
+let c =y/2;
+console.log(c);
+
+// Modulus
+let d =y%2;
+console.log(d);
+
+//Age
+console.log(typeof 29);
+
+//Age in text
+console.log  (typeof negenentwintig);
+ 
