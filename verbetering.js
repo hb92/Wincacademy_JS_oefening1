670ddbe597d296e2338074ddd4cc34f249942099
hb92 <britt.hellemans@gmail.com>
2021-12-07 14:00:06 +0100

    Add files via upload

---
+++ verbetering.js
@@ -0,0 +1,35 @@
+console.log("Hello Winc Academy")
+
+// Name 
+let name = 'Britt';
+console.log(name);
+
+// Nee een if variable gaat niet want is een keyword
+
+// Addition
+let sum = 3 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "3" + "4";
+console.log("sum with strings:", sumWithStrings);
+
+// Subtraction
+let subtraction = 10 - 3;
+console.log("subtraction:", subtraction);
+
+// Multiplication
+let multiplication = 1483 * 6342;
+console.log("multiplication:", multiplication);
+
+// Division
+let division = 12 / 3;
+console.log("division:", division);
+
+// Modulus
+let modulus = 12 % 9;
+console.log("modulus:", modulus);
+
+//Age
+let age = 29;
+console.log(typeof age);
+let ageAsString = "29";
+console.log(typeof ageAsString);
